a0a80f49f28b7b3319c4c4ca2f59170e5c98fcba
Kevin Wang <126712357+KevinWang676@users.noreply.github.com>
2025-03-05 16:56:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -340,4 +340,4 @@ async def startup_event():
 
 # Run the app
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("app:app", host="0.0.0.0", port=8030, reload=False)
